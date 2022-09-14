bbaebeb18eeab07e085b7d753a5db24c8add6114
lethanix <louismurguia@protonmail.com>
2022-09-13 21:35:53 -0500

    Initial Commit

---
+++ Serotonix/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Serotonix
+//
+//  Created by Louis Murguia on 13/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Serotonix/SerotonixApp.swift
@@ -0,0 +1,17 @@
+//
+//  SerotonixApp.swift
+//  Serotonix
+//
+//  Created by Louis Murguia on 13/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SerotonixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
